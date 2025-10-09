ae381e05bd2bf421e6f66ff280cf19dd118fc58e
AndraMaheswara <andram4heswara@gmail.com>
2025-10-09 09:18:07 +0700

    P6_Percobaan1

---
+++ P6/P6_Tugas1/ifCetakKRS27.java
@@ -0,0 +1,43 @@
+package P6.P6_Tugas1;
+import java.util.Scanner;
+public class ifCetakKRS27 {
+
+        public static void main(String[] args){
+        Scanner andra = new Scanner(System.in);
+
+        System.out.println("---Cetak KRS Siakad---");
+        System.out.print("Apakah UKT sudah lunas? (true/false): ");
+        boolean uktLunas27 = andra.nextBoolean();
+
+        if (uktLunas27) {
+                System.out.println("Pembayaran UKT erverifikasi");
+                System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+
+
+        }
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+}}
