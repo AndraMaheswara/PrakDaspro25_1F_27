e124ae0c37486a48c34246c004c5feea232e0428
AndraMaheswara <andram4heswara@gmail.com>
2025-10-09 10:10:04 +0700

    Modifikasi Percobaan 2

---
+++ P6/P6_Tugas1/ifElseCetakKRS27.java
@@ -0,0 +1,36 @@
+package P6.P6_Tugas1;
+import java.util.Scanner;
+public class ifElseCetakKRS27 {
+    
+
+             public static void main(String[] args){
+        Scanner andra = new Scanner(System.in);
+
+
+        System.out.println("---Cetak KRS Siakad---");
+        System.out.print("Masukan semester saat ini:");
+        int semester = andra.nextInt();
+
+
+              
+                    if (semester == 1)
+                    {System.out.println("KRS Semester 1 ditampilkan");}
+                    else if (semester ==2)
+                    {System.out.println("KRS Semester 2 ditampilkan");}
+                     else if (semester ==3)
+                    {System.out.println("KRS Semester 3 ditampilkan");}
+                     else if (semester ==4)
+                    {System.out.println("KRS Semester 4 ditampilkan");}
+                     else if (semester ==5)
+                    {System.out.println("KRS Semester 5 ditampilkan");}
+                     else if (semester ==6)
+                    {System.out.println("KRS Semester 6 ditampilkan");}
+                     else if (semester ==7)
+                    {System.out.println("KRS Semester 7 ditampilkan");}
+                     else if (semester ==8)
+                    {System.out.println("KRS Semester 8 ditampilkan");}
+                    else 
+                    {System.out.println("Semester tidak valid");}
+}
+}
+
