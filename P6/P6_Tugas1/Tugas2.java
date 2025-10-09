70c8561062c42124965f14b8c9e245566eec7937
AndraMaheswara <andram4heswara@gmail.com>
2025-10-09 14:29:35 +0700

    P6_Tugas2

---
+++ P6/P6_Tugas1/Tugas2.java
@@ -0,0 +1,27 @@
+package P6.P6_Tugas1;
+import java.util.Scanner;
+public class Tugas2 {
+    
+
+            public static void main(String[] args){
+            Scanner andra = new Scanner(System.in);
+
+
+                System.out.print("masukkan angka:");
+                int angka = andra.nextInt();
+
+                if (angka % 2 == 0){
+                System.out.println("angka genap");}
+
+                else {
+                    System.out.println("angka ganjil");
+                }
+
+
+
+
+
+
+
+
+             }}
