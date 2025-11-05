553c2b55c6d34df062dceaa4a9f544dedffa520b
RajendraPutraM <andram4heswara@gmail.com>
2025-11-05 19:24:58 +0700

    P10_Percobaan4_Pertanyaan2

--- P10/NilaiKelompok27.java
+++ P10/NilaiKelompok27.java
@@ -7,7 +7,10 @@ public class NilaiKelompok27 {
         Scanner andra = new Scanner(System.in);
 
         int kelompok27 = 1;
-        while (kelompok27 <= 6) {
+        double rataTertinggi27 = 0;
+        int kelompokTertinggi27 = 0;
+
+        while (kelompok27 <= 3) {
             System.out.println("Kelompok ke-" + kelompok27);
 
             double totalNilai27 = 0;
@@ -20,9 +23,17 @@ public class NilaiKelompok27 {
             double rataNilai27 = totalNilai27 / 5;
             System.out.println("Rata-rata nilai kelompok " + kelompok27 + " = " + rataNilai27);
             System.out.println();
-            kelompok27++;
+            
+
+            if (rataNilai27>rataTertinggi27) {
+                rataTertinggi27=rataNilai27;
+                kelompokTertinggi27=kelompok27;}
+                kelompok27++;
+
         }
+        System.out.println("Kelompok dengan rata-rata tertinggi adalah Kelompok ke-" + kelompokTertinggi27);
+        System.out.println("Dengan rata-rata nilai = " + rataTertinggi27);  
     }
 }
 
-//a
+// a
