5fc81fceb85c11d6f06ba4eac7268e55ca7de4cd
RajendraPutraM <andram4heswara@gmail.com>
2025-11-05 18:48:48 +0700

    P10_Percobaan2_Pertanyaan1

--- P10/Square27.java
+++ P10/Square27.java
@@ -9,7 +9,7 @@ public class Square27 {
         System.out.print("Masukkan nilai n = ");
         int n27 = andra.nextInt();
 
-        for (int iOuter27 = 1; iOuter27 <= n27; iOuter27++) {
+        for (int iOuter27 = 0; iOuter27 <= n27; iOuter27++) {
             for (int i27 = 1; i27 <= n27; i27++) {
                 System.out.print("*");
             }
