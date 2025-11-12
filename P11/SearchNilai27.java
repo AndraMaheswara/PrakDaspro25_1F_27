7294d11a5d1b07052413b5cae77ffb06f3788023
RajendraPutraM <andram4heswara@gmail.com>
2025-11-12 21:57:43 +0700

    P11 Percobaan4 Pertanyaan2

--- P11/SearchNilai27.java
+++ P11/SearchNilai27.java
@@ -1,23 +1,34 @@
 package P11;
-
+import java.util.Scanner;
 public class SearchNilai27 {
     public static void main(String[] args) {
-        int[] arrNilai27 = {80,85,78,96,90,82,86};
-        int key27 = 90;
+        Scanner andra = new Scanner(System.in);
+        
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        int jumlahNilai27 = andra.nextInt();
+        int[] arrNilai27 = new int[jumlahNilai27];
+
+        for (int i = 0; i < arrNilai27.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            arrNilai27[i] = andra.nextInt();
+        }
+
+
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        int key27 = andra.nextInt();
         int hasil27 = -1; 
 
-        
         for (int i = 0; i < arrNilai27.length; i++) {
             if (key27 == arrNilai27[i]) {
                 hasil27 = i;
                 break;
             }
         }
-
+        
         System.out.println();
         if (hasil27 != -1) {
-            System.out.println("Nilai " + key27 + " ketemu di indeks ke-" + hasil27);
-        }
-        System.out.println();
+            System.out.println("Nilai " + key27 + " ketemu di indeks ke-" + hasil27);}
+        
+         
     }
 }
