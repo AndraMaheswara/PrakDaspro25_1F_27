85b40b7c5432760ac2758391907f100b7841ff04
RajendraPutraM <andram4heswara@gmail.com>
2025-11-12 20:55:02 +0700

    P11 Percobaan4

---
+++ P11/SearchNilai27.java
@@ -0,0 +1,28 @@
+package P11;
+
+public class SearchNilai27 {
+    public static void main(String[] args) {
+        int[] arrNilai27 = {80,85,78,96,90,82,86};
+        int key27 = 90;
+        int hasil27 = -1; 
+
+        
+        for (int i = 0; i < arrNilai27.length; i++) {
+            if (key27 == arrNilai27[i]) {
+                hasil27 = i;
+                break;
+            }
+        }
+
+        System.out.println();
+        if (hasil27 != -1) {
+            System.out.println("Nilai " + key27 + " ketemu di indeks ke-" + hasil27);
+        } else {
+            System.out.println("Nilai " + key27 + " tidak ditemukan dalam array.");
+        }
+        System.out.println();
+    }
+}
+
+    
+
