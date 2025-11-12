bd391d2175669301e77c62aeb2f83a6c1169a604
RajendraPutraM <andram4heswara@gmail.com>
2025-11-12 21:25:16 +0700

    P11 Percobaan3 Pertanyaan1

--- P11/ArrayRataNilai27.java
+++ P11/ArrayRataNilai27.java
@@ -10,21 +10,28 @@ public class ArrayRataNilai27 {
         int[] nilaiMhs27 = new int[10];
         double total27 = 0;
         double rata27;
-
+        int jumlahLulus27 = 0;
         
+        
+
         for (int i = 0; i < nilaiMhs27.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
             nilaiMhs27[i] = andra.nextInt();
         }
 
-   
+        for (int i = 0; i < nilaiMhs27.length; i++) {
+            total27 += nilaiMhs27[i];
+            if (nilaiMhs27[i] > 70) {
+                jumlahLulus27++;
+            }
+        }
+
         for (int i = 0; i < nilaiMhs27.length; i++) {
             total27 += nilaiMhs27[i];
         }
 
-     
         rata27 = total27 / nilaiMhs27.length;
         System.out.println("Rata-rata nilai = " + rata27);
-
+        System.out.println("Jumlah mahasiswa yang lulus = " + jumlahLulus27);   
     }
 }
