51dcf158fdd48c93914b2a1a3146be9d3cadf77c
RajendraPutraM <andram4heswara@gmail.com>
2025-11-12 20:55:58 +0700

    P11 Percobaan1 Pertanyaan1

--- P11/ArrayBilangan27.java
+++ P11/ArrayBilangan27.java
@@ -6,10 +6,10 @@ public static void main(String[] args){
 
 
 int[] bil27 = new int[4];
-bil27[0]=5;
-bil27[1]=13;
-bil27[2]=-7;
-bil27[3]=17;
+bil27[0]=5.0;
+bil27[1]=12867;
+bil27[2]=7.5;
+bil27[3]=20000;
 
 System.out.println(bil27[0]);
 System.out.println(bil27[1]);
