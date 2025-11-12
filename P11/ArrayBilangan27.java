ba1dd4c1409c241afc9c6e51b56896d5efc09038
RajendraPutraM <andram4heswara@gmail.com>
2025-11-12 20:52:01 +0700

    P11 Percobaan1

---
+++ P11/ArrayBilangan27.java
@@ -0,0 +1,20 @@
+package P11;
+
+public class ArrayBilangan27 { 
+
+public static void main(String[] args){
+
+
+int[] bil27 = new int[4];
+bil27[0]=5;
+bil27[1]=13;
+bil27[2]=-7;
+bil27[3]=17;
+
+System.out.println(bil27[0]);
+System.out.println(bil27[1]);
+System.out.println(bil27[2]);
+System.out.println(bil27[3]);
+
+    
+}}
