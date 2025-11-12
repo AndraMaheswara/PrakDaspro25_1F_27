033ff35df44e1a13b4b4f995706dd11af47e01ac
RajendraPutraM <andram4heswara@gmail.com>
2025-11-12 20:54:13 +0700

    P11 Percobaan3

---
+++ P11/ArrayRataNilai27.java
@@ -0,0 +1,30 @@
+package P11;
+
+import java.util.Scanner;
+
+public class ArrayRataNilai27 {
+    public static void main(String[] args) {
+        Scanner andra = new Scanner(System.in);
+
+  
+        int[] nilaiMhs27 = new int[10];
+        double total27 = 0;
+        double rata27;
+
+        
+        for (int i = 0; i < nilaiMhs27.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
+            nilaiMhs27[i] = andra.nextInt();
+        }
+
+   
+        for (int i = 0; i < nilaiMhs27.length; i++) {
+            total27 += nilaiMhs27[i];
+        }
+
+     
+        rata27 = total27 / nilaiMhs27.length;
+        System.out.println("Rata-rata nilai = " + rata27);
+
+    }
+}
