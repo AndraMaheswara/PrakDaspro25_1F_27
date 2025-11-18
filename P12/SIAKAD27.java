a6dd57b763e6d4d8f161c2f2775b9b2dd5541b9c
RajendraPutraM <andram4heswara@gmail.com>
2025-11-18 22:37:04 +0700

    P12 Percobaan4 Pertanyaan1

--- P12/SIAKAD27.java
+++ P12/SIAKAD27.java
@@ -4,19 +4,25 @@ import java.util.Scanner;
 public class SIAKAD27 {
     public static void main(String[] args) {
         Scanner andra = new Scanner(System.in);
-            int[][] nilai27 = new int[4][3];
+            System.out.print("Masukkan jumlah siswa: ");
+            int jumlahSiswa27 = andra.nextInt();
 
-            for (int i = 0; i < nilai27.length; i++) {
-                System.out.println("Input nilai mahasiswa ke-" + (i + 1));
-                double totalPerSiswa27 = 0;
+            System.out.print("Masukkan jumlah mata kuliah: ");
+            int jumlahMatkul27 = andra.nextInt();
+            
+            int[][] nilai27 = new int[jumlahSiswa27][jumlahMatkul27];
 
-                for (int j = 0; j < nilai27[i].length; j++) {
-                    System.out.print("Nilai mata kuliah " + (j + 1) + ": ");
-                    nilai27[i][j] = andra.nextInt();
-                    totalPerSiswa27 += nilai27[i][j];
-                }
+            for (int i = 0; i < jumlahSiswa27; i++) {
+            System.out.println("\nInput nilai mahasiswa ke-" + (i + 1));
+            double totalPerSiswa27 = 0;
 
-                System.out.println("Nilai rata-rata: " + totalPerSiswa27 / 3);
+            for (int j = 0; j < jumlahMatkul27; j++) {
+                System.out.print("Nilai mata kuliah " + (j + 1) + ": ");
+                nilai27[i][j] = andra.nextInt();
+                totalPerSiswa27 += nilai27[i][j];
+            }
+
+                System.out.println("Nilai rata-rata: " + totalPerSiswa27 / jumlahMatkul27);
             }
             System.out.println("\n=============================");
             System.out.println("Rata-rata Nilai Setiap mata kuliah: ");
@@ -28,7 +34,7 @@ public class SIAKAD27 {
                     totalPerMatkul27 += nilai27[i][j];
                     
                 }
-                System.out.println("Mata kuliah " + (j+1)+": " +totalPerMatkul27/4);
+                System.out.println("Mata kuliah " + (j+1)+": " +totalPerMatkul27/jumlahSiswa27);
             }
         
     }
