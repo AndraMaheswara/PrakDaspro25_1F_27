1c271dc1d3f8aadc9d49f15b549f43a89a869c6c
RajendraPutraM <andram4heswara@gmail.com>
2025-11-18 19:24:20 +0700

    P12 Percobaan1 Pertanyaan6

--- P12/Bioskop27.java
+++ P12/Bioskop27.java
@@ -17,11 +17,12 @@ public class Bioskop27 {
          
 
         System.out.println(penonton27.length);
-        for (int i = 0; i < penonton27.length; i++) {
-            System.out.println("Panjang baris ke-" +(i+1)+": " +penonton27[i].length);
+        for (String[] barisPenonton27 : penonton27) {
+            System.out.println("Panjang baris: " + barisPenonton27.length);
         }
+        
        
-       
+    
         System.out.printf("%s\t%s\n", penonton27[0][0], penonton27[0][1]);
         System.out.printf("%s\t%s\n", penonton27[1][0], penonton27[1][1]);
         System.out.printf("%s\t%s\n", penonton27[2][0], penonton27[2][1]);
