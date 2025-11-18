0c4dbe9a135d64825ea530a3dbeb9982d035c12b
RajendraPutraM <andram4heswara@gmail.com>
2025-11-18 22:19:15 +0700

    P12 Percobaan3 Pertanyaan 1

--- P12/Numbers27.java
+++ P12/Numbers27.java
@@ -1,11 +1,18 @@
 package P12;
 
+import java.util.Arrays;
+
 public class Numbers27 {
      public static void main(String[] args) {
-        int[][] myNumbers = new int[3][];
-        myNumbers[0] = new int[5];
-        myNumbers[1] = new int[3];
-        myNumbers[2] = new int[1];
+        int[][] myNumbers27 = new int[3][];
+        myNumbers27[0] = new int[5];
+        myNumbers27[1] = new int[3];
+        myNumbers27[2] = new int[1];
+    
+
+        for (int i = 0; i < myNumbers27.length; i++) {
+            System.out.println(Arrays.toString(myNumbers27[i]));
+        }
     }
 }
 
