1d25cc04c90c6395d2ad979551784f375c1e2f78
RajendraPutraM <andram4heswara@gmail.com>
2025-12-03 22:44:35 +0700

    P14 Percobaan5 langkah3

--- P14/HitungBalok27.java
+++ P14/HitungBalok27.java
@@ -14,12 +14,19 @@ public class HitungBalok27 {
         System.out.println("Masukkan tinggi");
         t27 = andra.nextInt();
 
-        L27 = p27 * l27;
+        L27 = hitungLuas27(p27, l27);
         System.out.println("Luas Persegi Panjang adalah " + L27);
 
-        vol27 = p27 * l27 * t27;
+        vol27 = hitungVolume27(t27, p27, l27);
         System.out.println("Volume Balok adalah " + vol27);
         
-        
+        andra.close();
     }
-}
+    static int hitungLuas27(int pjg27, int lb27) {
+        int Luas27 = pjg27 * lb27;
+        return Luas27;
+    }
+    static int hitungVolume27(int tinggi27, int a27, int b27) {
+        int volume27 = hitungLuas27(a27, b27) * tinggi27;
+        return volume27;
+    }}
