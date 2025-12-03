5d6ee949dad989ce01b844633be661c0d4d65ec4
RajendraPutraM <andram4heswara@gmail.com>
2025-12-03 20:28:56 +0700

    P14 Percobaan2

--- P14/Kafe27.java
+++ P14/Kafe27.java
@@ -3,6 +3,20 @@ package P14;
 public class Kafe27 {
     
     public static void main(String[] args) {
+        Menu27("Andi",true);
+    }
+
+
+    public static void Menu27(String namaPelanggan27, boolean isMember27) {
+        System.out.println();
+        System.out.println("Selamat datang " +namaPelanggan27+ "!");
+        
+
+        if (isMember27){
+            System.out.println("Anda adalah member, dapat diskon 10% untuk setiap pembelian!");
+        }
+
+        System.out.println();
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam   Rp 15,000");
         System.out.println("2. Cappuccino   Rp 20,000");
@@ -12,11 +26,6 @@ public class Kafe27 {
         System.out.println("6. Mie Goreng   Rp 18,000");
         System.out.println("===========================");
         System.out.println("Silakan pilih menu yang Anda inginkan.");
-    }
-
-
-    public static void Menu27() {
-        
     }
 
     
